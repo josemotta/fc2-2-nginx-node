a4de965024266d1c346b7260d6eff362babe9808
Jose Motta <josemotta@bampli.com>
2021-02-23 19:23:05 -0300

    revised nginx.conf & index.js

--- node/index.js
+++ node/index.js
@@ -11,19 +11,50 @@ const mysql = require('mysql')
 const connection = mysql.createConnection(config)
 console.log('Created connection.')
 
-const sql1 = `CREATE TABLE IF NOT EXISTS people(id int not null auto-increment, name varchar(255), primary key(id))`
+const sql1 = `CREATE TABLE IF NOT EXISTS people(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255))`
 connection.query(sql1)
 console.log('Certified that people table exists.')
 
 const sql2 = `INSERT INTO people(name) values('Wesley')`
 connection.query(sql2)
-console.log('Added person to people.')
-
+console.log('Added another person to people.')
 connection.end()
-console.log('Closed connection.')
+
+getPeople = async function(connection){
+    return new Promise(function(resolve, reject){
+        connection.query(
+            "SELECT id, name FROM people", 
+            function(err, rows){                                                
+                if(rows === undefined){
+                    reject(new Error("Error reading people"));
+                }else{
+                    resolve(rows);
+                }
+            }
+        )}
+    )}
 
 app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle Rocks!</h1>')
+    const conn = mysql.createConnection(config)
+    
+    if(req.method == 'GET'){
+        if(req.url == '/'){
+            res.setHeader('Content-type', 'text/html');
+            getPeople(conn)
+            .then(function(results){
+                html = "<h1>Full Cycle Rocks!</h1>"
+                html += "<h2>"+results.length+" people found</h2>"
+                html += "<ul>"
+                for (var i in results) html += "<li>" + results[i].id + " " +results[i].name + "</li>";
+                html += "</ul>"
+                res.end(html);
+            })
+            .catch(function(err){
+                console.log("Promise rejection error: "+err);
+                res.end("<h1>ERROR</h1>")
+            })
+        }
+    }
 })
 
 app.listen(port, ()=> {
